8ef5505d0cfa57be2dc28c52e3db0e07402b07cb
Artem Babak <dev@tararoutray.com>
2023-12-09 02:50:50 +0300

    fix call size in eer

--- antispoof/metric/eer.py
+++ antispoof/metric/eer.py
@@ -8,10 +8,10 @@ class EER(BaseMetric):
 
     def compute_det_curve(self, target_scores, nontarget_scores):
 
-        n_scores = target_scores.size + nontarget_scores.size
+        n_scores = target_scores.size() + nontarget_scores.size()
         all_scores = np.concatenate((target_scores, nontarget_scores))
         labels = np.concatenate(
-            (np.ones(target_scores.size), np.zeros(nontarget_scores.size)))
+            (np.ones(target_scores.size()), np.zeros(nontarget_scores.size())))
 
         # Sort labels based on scores
         indices = np.argsort(all_scores, kind='mergesort')
@@ -19,14 +19,14 @@ class EER(BaseMetric):
 
         # Compute false rejection and false acceptance rates
         tar_trial_sums = np.cumsum(labels)
-        nontarget_trial_sums = nontarget_scores.size - \
+        nontarget_trial_sums = nontarget_scores.size() - \
             (np.arange(1, n_scores + 1) - tar_trial_sums)
 
         # false rejection rates
         frr = np.concatenate(
-            (np.atleast_1d(0), tar_trial_sums / target_scores.size))
+            (np.atleast_1d(0), tar_trial_sums / target_scores.size()))
         far = np.concatenate((np.atleast_1d(1), nontarget_trial_sums /
-                            nontarget_scores.size))  # false acceptance rates
+                            nontarget_scores.size()))  # false acceptance rates
         # Thresholds are the sorted scores
         thresholds = np.concatenate(
             (np.atleast_1d(all_scores[indices[0]] - 0.001), all_scores[indices]))
